bc6c42249bd0f3a78523a443b38886af9fc91297
Hordur Freyr Yngvason <hordurf@gmail.com>
2022-12-02 11:13:56 +0000

    Solve day 2

---
+++ day-2.lisp
@@ -0,0 +1,65 @@
+(defpackage :advent-of-code-2022/day-2
+  (:use :cl)
+  (:import-from #:uiop
+                #:read-file-string)
+  (:import-from #:serapeum
+                #:words
+                #:~>
+                #:lines)
+  (:export
+   :load-input
+   :part-1
+   :part-2
+   :*example*))
+
+(in-package :advent-of-code-2022/day-2)
+
+(defparameter *example* "A Y
+B X
+C Z")
+
+(defun load-input ()
+  (read-file-string "day-2.input"))
+
+(defun parse-line (line)
+  (destructuring-bind (a x) (words line)
+    (list (position a '("A" "B" "C") :test 'equal)
+          (position x '("X" "Y" "Z") :test 'equal))))
+
+(defun parse-input (input)
+  (~> (lines input)
+      (mapcar #'parse-line _)))
+
+(defun round<= (self other)
+  (= (winner self) other))
+
+(defun winner (self)
+  (mod (1+ self) 3))
+
+(defun loser (self)
+  (mod (1- self) 3))
+
+(defun simple-score (round)
+  (destructuring-bind (elf me) round
+    (if (= elf me)
+        (+ 3 (1+ me))
+        (if (round<= elf me)
+            (+ 6 (1+ me))
+            (1+ me)))))
+
+(defun part-1 (input)
+  (~> (parse-input input)
+      (mapcar #'simple-score _)
+      (reduce #'+ _)))
+
+(defun strategy-score (round)
+  (destructuring-bind (elf outcome) round
+    (ecase outcome
+      (0 (1+ (loser elf)))
+      (1 (+ 3 (1+ elf)))
+      (2 (+ 6 (1+ (winner elf)))))))
+
+(defun part-2 (input)
+  (~> (parse-input input)
+      (mapcar #'strategy-score _)
+      (reduce #'+ _)))
--- tests.lisp
+++ tests.lisp
@@ -1,6 +1,7 @@
 (defpackage :advent-of-code-2022-tests/tests
   (:use :cl :fiveam)
-  (:local-nicknames (:day-1 :advent-of-code-2022/day-1)))
+  (:local-nicknames (:day-1 :advent-of-code-2022/day-1))
+  (:local-nicknames (:day-2 :advent-of-code-2022/day-2)))
 
 (in-package :advent-of-code-2022-tests/tests)
 
@@ -9,3 +10,11 @@
 (test regression-day-1
   (is (= 71924 (day-1:part-1 (day-1:load-input))))
   (is (= 210406 (day-1:part-2 (day-1:load-input)))))
+
+(test unit-day-2
+  (is (= 15 (day-2:part-1 day-2:*example*)))
+  (is (= 12 (day-2:part-2 day-2:*example*))))
+
+(test regression-day-2
+  (is (= 11873 (day-2:part-1 (day-2:load-input))))
+  (is (= 12014 (day-2:part-2 (day-2:load-input)))))
