9608b85f0c0d34e9714f3bf73d5d23bf2e2ac3e6
Hordur Freyr Yngvason <hordurf@gmail.com>
2022-12-21 03:43:14 +0000

    Solve day 17 part 2
    This was a fiddly one. Had the right idea from the start but took a long
    time to get the little details right :/

--- day-17.lisp
+++ day-17.lisp
@@ -17,19 +17,74 @@
          (cols (length (car lines))))
     (make-array (list rows cols) :initial-contents lines :element-type 'character)))
 
-(defun simulate-tower (jets rock-count)
-  (let* ((cols 7)
-         (rows (* 4 rock-count))
-         (rocks (parse-rock-types *rock-types-string*))
-         (tower (make-array (list rows cols) :element-type 'character :initial-element #\.))
-         (top 0)
-         (jet-index 0))
+(defun simulate-tower (jets rock-count &optional
+                                         (tower (make-array (list (* 4 rock-count) 7) :element-type 'character :initial-element #\.))
+                                         (top 0)
+                                         (rock-offset 0)
+                                         (jet-index 0))
     (dotimes (iteration rock-count)
-      (let ((rock (nth (mod iteration (length rocks)) rocks)))
+      (let ((rock (nth (mod (+ rock-offset iteration) (length *rocks*)) *rocks*)))
         (multiple-value-bind (next-top next-jet-index) (simulate-rock tower rock jets top jet-index)
           (setf top next-top
                 jet-index next-jet-index))))
-    (values top tower)))
+    (values top tower))
+
+(defun part-1 (input)
+  (nth-value 0 (simulate-tower input 2022)))
+
+(defun find-tower-bottom (tower bottom top)
+  (loop for col from 0 to 6
+        minimizing
+        (loop for row from (1+ top) downto bottom
+              until (or (= row bottom)
+                        (char= #\# (aref tower (1- row) col)))
+              finally (return row))))
+
+(defun part-2 (input &optional (rock-count 1000000000000))
+  (let* ((rec (find-tower-recurrence input))
+         (cycle-start (getf rec :next-seen))
+         (cycle-length (- (getf rec :next-seen)
+                          (getf rec :first-seen)))
+         (top (getf rec :next-top))
+         (tower-growth (- top
+                          (getf rec :first-top)))
+         (tower (getf rec :tower))
+         (jet-index (getf rec :jet-index)))
+    (multiple-value-bind (cycle-count remaining-rocks) (floor (- rock-count cycle-start 1) cycle-length)
+      (let ((final-top (simulate-tower input remaining-rocks tower top (1+ cycle-start) jet-index)))
+        (+ final-top (* cycle-count tower-growth))))))
+
+(defun find-tower-recurrence (jets &optional (rock-count 5000))
+  (let* ((cols 7)
+         (rows (* 4 rock-count))
+         (tower (make-array (list rows cols) :element-type 'character :initial-element #\.))
+         (top 0)
+         (bottom 0)
+         (jet-index 0)
+         (seen (make-hash-table :test 'equal)))
+    (declare (fixnum rock-count cols rows top bottom jet-index))
+    (dotimes (iteration rock-count)
+      (let* ((rock-index (mod iteration (length *rocks*)))
+             (rock (nth rock-index *rocks*)))
+        (multiple-value-bind (next-top next-jet-index) (simulate-rock tower rock jets top jet-index)
+          (setf top next-top
+                bottom (find-tower-bottom tower bottom top)
+                jet-index next-jet-index)
+          (let* ((key (list rock-index
+                            jet-index
+                            (with-output-to-string (stream)
+                              (print-tower tower top bottom stream))))
+                 (last-iteration (gethash key seen)))
+            (when last-iteration
+              (return-from find-tower-recurrence (list
+                                                  :first-seen (car last-iteration)
+                                                  :first-top (cadr last-iteration)
+                                                  :next-seen iteration
+                                                  :next-top top
+                                                  :jet-index jet-index
+                                                  :tower tower)))
+            (setf (gethash key seen) (list iteration top))))))))
+
 
 (-> rock-intersect-p (simple-array simple-array fixnum fixnum) boolean)
 (defun rock-intersect-p (tower rock row col)
@@ -78,19 +133,21 @@ Otherwise, returns `nil'."
       (values (max top (+ row rock-rows))
               jet-index))))
 
-(defun print-tower (tower top &optional (stream *standard-output*))
-  (loop for row from top downto 0 do
+(defun print-tower (tower top &optional (bottom 0) (stream *standard-output*))
+  (loop for row from top downto bottom do
     (loop for col from 0 upto 6 do
       (write-char (aref tower row col) stream)
-      finally (write-char #\Newline))))
-
-(defun parse-rock-types (string)
-  (~> (ppcre:split "\\n\\n" string)
-      (mapcar #'grid-from-string _)))
+      finally (write-char #\Newline stream))))
 
 (defun load-input ()
   (trim-whitespace (read-file-string "day-17.input")))
 
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defun parse-rock-types (string)
+    (~> (ppcre:split "\\n\\n" string)
+        (mapcar #'grid-from-string _))))
+
+(defparameter *rocks* (parse-rock-types *rock-types-string*))
 (defparameter *rock-types-string* "####
 
 .#.
@@ -111,3 +168,5 @@ Otherwise, returns `nil'."
 
 (defparameter *example*
   ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>")
+; 1559887005677
+; 1514285714289
