b8ac4d896e3a06fa465147bb2684fa26eba275e6
Hordur Freyr Yngvason <hordurf@gmail.com>
2023-02-08 23:10:02 +0000

    Kind-of solve day 22 part 2
    It works on my input but is off-by-one on the example.

--- day-22.lisp
+++ day-22.lisp
@@ -4,13 +4,17 @@
                 #:read-file-string)
   (:import-from #:ppcre)
   (:import-from #:serapeum
+                #:do-hash-table
                 #:lines)
   (:import-from #:alexandria
+                #:curry
                 #:if-let)
   (:import-from #:trivia
                 #:ematch))
 
 (in-package :advent-of-code-2022/day-22)
+
+(defconstant +cube-facet-neighbors+ 4)
 
 (defstruct (board (:constructor board (rows cols &aux (data (make-array (list rows cols) :initial-element #\Space)))))
   (data nil :type simple-array))
@@ -20,6 +24,12 @@
   (board nil :type board)
   (direction (list 0 1) :type list)
   (position nil :type list))
+
+(defstruct (cubic-state (:include state)
+                        (:copier nil))
+  (edge-length 1 :type integer)
+  (facet-corner-table nil :type hash-table)
+  (corner-facet-table nil :type hash-table))
 
 (defun state-row (state)
   (car (state-position state)))
@@ -34,10 +44,19 @@
     ((list 0 -1) 2)
     ((list -1 0) 3)))
 
-(defun copy-state (state)
+(defmethod copy-state ((state state))
   (state (state-board state)
          (copy-list (state-direction state))
          (copy-list (state-position state))))
+
+(defmethod copy-state ((state cubic-state))
+  (make-cubic-state
+   :board (state-board state)
+   :direction (copy-list (state-direction state))
+   :position (copy-list (state-position state))
+   :facet-corner-table (cubic-state-facet-corner-table state)
+   :corner-facet-table (cubic-state-corner-facet-table state)
+   :edge-length (cubic-state-edge-length state)))
 
 (defun starting-position (board)
   (dotimes (row (board-rows board))
@@ -55,7 +74,7 @@
 (defun list- (first &rest rest)
   (apply #'mapcar #'- first rest))
 
-(defun advance-state (state)
+(defmethod advance-state ((state state))
   (let* ((board (state-board state))
          (direction (state-direction state))
          (prev-position (state-position state))
@@ -70,13 +89,21 @@
                next-position)
         state)))
 
+(defun direction-char (direction)
+  (ematch direction
+    ('(-1 0) #\^)
+    ('(1 0) #\v)
+    ('(0 -1) #\<)
+    ('(0 1) #\>)))
+
 (defun apply-move (state move)
   (let ((next-state (copy-state state)))
-    (setf (apply #'bref (state-board state) (state-position next-state)) #\x)
     (etypecase move
       (number (dotimes (i move)
-                (setf next-state (advance-state next-state))
-                (setf (apply #'bref (state-board state) (state-position next-state)) #\x)))
+                (print (state-position next-state))
+                (setf (apply #'bref (state-board next-state) (state-position next-state))
+                      (direction-char (state-direction next-state)))
+                (setf next-state (advance-state next-state))))
       (string (setf (state-direction next-state)
                     (ematch move
                       ("L" (ematch (state-direction state)
@@ -99,13 +126,29 @@
         (write-char (bref obj row col) stream))
       (write-char #\Newline stream))))
 
-(defun final-password (board moves)
-  (let ((state (state board)))
-    (dolist (move moves)
-      (setf state (apply-move state move)))
-    (+ (* 1000 (1+ (state-row state)))
-       (* 4 (1+ (state-col state)))
-       (state-facing state))))
+(defun final-password (state moves)
+  (dolist (move moves)
+    (setf state (apply-move state move)))
+  (print (state-board state))
+  (+ (* 1000 (1+ (state-row state)))
+     (* 4 (1+ (state-col state)))
+     (state-facing state)))
+
+(defun part-1 (input)
+  (destructuring-bind (board moves) (parse-input input)
+    (let ((state (state board)))
+      (final-password state moves))))
+
+(defun part-2 (input &optional (edge-length 50))
+  (destructuring-bind (board moves) (parse-input input)
+    (let* ((corner-facet-table (fold-cube board edge-length))
+           (state (make-cubic-state
+                   :board board
+                   :position (starting-position board)
+                   :facet-corner-table (hash-table-inverse corner-facet-table)
+                   :corner-facet-table corner-facet-table
+                   :edge-length edge-length)))
+      (final-password state moves))))
 
 (defun board-rows (board)
   (array-dimension (board-data board) 0))
@@ -145,29 +188,130 @@
   (when (= n 0)
     (return-from rotate-clockwise list))
   (let ((rotated-once (destructuring-bind (row col) list
-                        (list (- col) row))))
+                        (list col (- row)))))
     (rotate-clockwise rotated-once (1- n))))
 
-(defun unfold-cube (board cube-edge-length)
+(defun fold-cube (board cube-edge-length)
   (do* ((corner-0 (starting-position board))
-        (center-0 (list+ corner-0 (list (/ cube-edge-length 2)
-                                        (/ cube-edge-length 2))))
         (direction-0 (list (- cube-edge-length) 0))
-        (visited (make-hash-table :test 'equal))
-        (stack (list (list :-z :-y center-0 direction-0))))
-       ((not stack) visited)
+        (corner-facet-table (make-hash-table :test 'equal))
+        (stack (list (list :-z :-y corner-0 direction-0))))
+       ((not stack) corner-facet-table)
     (destructuring-bind (facet previous-facet position direction) (pop stack)
-      (setf (gethash position visited) facet)
+      (setf (gethash position corner-facet-table) facet)
       (let* ((neighbors (cube-neighbors facet))
              (offset (+ 2 (position previous-facet neighbors))))
-        (dotimes (i 4)
-          (let ((next-facet (nth (mod (+ i offset) 4) neighbors))
+        (dotimes (i +cube-facet-neighbors+)
+          (let ((next-facet (nth (mod (+ i offset) +cube-facet-neighbors+) neighbors))
                 (next-position (list+ position direction)))
-            (print next-position)
             (unless (or (apply #'out-of-bounds-p board next-position)
-                        (gethash next-position visited))
+                        (gethash next-position corner-facet-table))
               (push (list next-facet facet next-position direction) stack))
             (setf direction (rotate-clockwise direction 1))))))))
+
+(defun hash-table-inverse (table &key (test 'equal))
+  (let ((inverse (make-hash-table :test test)))
+    (do-hash-table (key value table inverse)
+      (setf (gethash value inverse) key))))
+
+(defun manhattan-distance (first second)
+  (loop for x in first
+        for y in second
+        summing (abs (- x y))))
+
+(defun nearest-facet (position facet-corner-table edge-length)
+  (let ((best-corner nil)
+        (best-facet nil)
+        (best-distance edge-length))
+    (do-hash-table (facet corner facet-corner-table)
+      (let ((distance (manhattan-distance (mapcar (curry #'+ (/ edge-length 2)) corner)
+                                          position)))
+        (when (<= distance best-distance)
+          (setf best-corner corner
+                best-facet facet
+                best-distance distance))))
+    (values best-corner best-facet)))
+
+(defmethod advance-state ((state cubic-state))
+  (let* ((board (state-board state))
+         (direction (state-direction state))
+         (position (state-position state))
+         (facet-corner-table (cubic-state-facet-corner-table state))
+         (corner-facet-table (cubic-state-corner-facet-table state))
+         (next-position (list+ direction (state-position state)))
+         (next-direction direction)
+         (edge-length (cubic-state-edge-length state)))
+    (when (apply #'out-of-bounds-p board next-position)
+      (multiple-value-bind (corner facet)
+          (nearest-facet position facet-corner-table edge-length)
+        (assert corner)
+        (let ((neighbor-facet (find-neighbor-facet direction corner facet facet-corner-table edge-length)))
+          (setf next-direction (mapcar (curry #'* -1) (find-edge-direction neighbor-facet facet facet-corner-table edge-length)))
+          (let* ((rotation-offset (rotation-offset direction next-direction))
+                 (corner->next-position (list- next-position corner))
+                 (rotated-corner->next-position (rotate-clockwise corner->next-position rotation-offset))
+                 (neighbor-corner (gethash neighbor-facet facet-corner-table))
+                 (rotated-corner (list+ (list- neighbor-corner
+                                               (mapcar (curry #'* edge-length) next-direction))
+                                        (ecase rotation-offset
+                                          (0 (list 0 0))
+                                          (1 (list 0 (1- edge-length)))
+                                          (2 (list (1- edge-length) (1- edge-length)))
+                                          (3 (list (1- edge-length) 0))))))
+
+            (setf next-position (list+ rotated-corner
+                                       rotated-corner->next-position))
+            (print (list "neighbor-corner" neighbor-corner
+                         "rotated-corner" rotated-corner
+                         "rotated-corner->next-position" rotated-corner->next-position
+                         "next-position" next-position))))))
+    (assert (not (apply #'out-of-bounds-p board next-position)))
+    (if (char/= #\# (apply #'bref board next-position))
+        (make-cubic-state
+         :board board
+         :direction next-direction
+         :position next-position
+         :edge-length edge-length
+         :facet-corner-table facet-corner-table
+         :corner-facet-table corner-facet-table)
+        state)))
+
+(defun find-edge-direction (source-facet target-facet facet-corner-table edge-length)
+  (let ((source-center (gethash source-facet facet-corner-table)))
+    (multiple-value-bind (anchor-facet anchor-center anchor-index)
+        (find-anchor-facet source-center source-facet facet-corner-table edge-length)
+      (declare (ignore anchor-facet))
+      (assert anchor-index)
+      (let* ((neighbors (cube-neighbors source-facet))
+             (anchor-direction (mapcar #'signum (list- anchor-center source-center)))
+             (target-index (position target-facet neighbors)))
+        (assert target-index)
+        (rotate-clockwise anchor-direction (mod (- target-index anchor-index) +cube-facet-neighbors+))))))
+
+(defun rotation-offset (source target)
+  (loop for i from 0 upto +cube-facet-neighbors+
+        for d = source then (rotate-clockwise d 1)
+        when (equal d target) do
+          (return i)))
+
+(defun find-anchor-facet (corner facet facet-corner-table edge-length)
+  "Returns a neighbor facet that is also adjacent in the folded cube"
+  (loop with neighbors = (cube-neighbors facet)
+        for anchor-facet in neighbors
+        for anchor-corner = (gethash anchor-facet facet-corner-table)
+        for anchor-index = 0 then (1+ anchor-index)
+        when (= edge-length (manhattan-distance anchor-corner corner)) do
+          (return (values anchor-facet anchor-corner anchor-index))))
+
+(defun find-neighbor-facet (direction corner facet facet-corner-table edge-length)
+  (multiple-value-bind (anchor-facet anchor-corner anchor-index)
+      (find-anchor-facet corner facet facet-corner-table edge-length)
+    (declare (ignore anchor-facet))
+    (let* ((anchor-direction (mapcar #'signum (list- anchor-corner corner)))
+           (offset (rotation-offset anchor-direction direction))
+           (neighbor-index (mod (+ offset anchor-index) +cube-facet-neighbors+))
+           (neighbor-facet (nth neighbor-index (cube-neighbors facet))))
+      neighbor-facet)))
 
 (defun parse-moves (string)
   (loop with start = 0
